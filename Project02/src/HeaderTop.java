0c478dce2b340addb82e44a2ec991d62385a4c7a
nhbsohel2 <nhbsohel@gmail.com>
2019-08-26 13:34:48 +0600

    dropdown dollar selected

--- Project02/src/HeaderTop.java
+++ Project02/src/HeaderTop.java
@@ -1,17 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.TimeUnit;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 class HeaderTop {
 
 	
@@ -44,6 +44,27 @@ class HeaderTop {
 		// Verify if email text is correct
 		assertEquals(emailDIV.getText(), "info@travelagency.com");
 		driver.quit();
+
+	}
+	
+	
+	@Test
+	public void isUSDollarSelected() {
+		System.setProperty("webdriver.gecko.driver","D:\\fire\\geckodriver.exe");
+		WebDriver driver =new FirefoxDriver();
+		driver.get("http://phptravels.net/");
+
+		// Find dropdown menu of currency
+		WebElement currency_dropdown = driver.findElement(By.id("currency"));
+
+		// Create new select object
+		Select currency = new Select(currency_dropdown);
+
+		// Get first selected option
+		WebElement firstSelected = currency.getFirstSelectedOption();
+
+		// Verify if "US Dollar" selected
+		assertEquals(firstSelected.getText(), "US Dollar");
 
 	}
 	
